175f4aeacefaae08b314ccf6ccf51307b7c190a3
luisin9878 <74559482+luisin9878@users.noreply.github.com>
2021-03-29 18:37:06 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,7 +9,8 @@ const routes = [
       { path: '/cadastroAnimais', component: () => import('pages/cadastroAnimais.vue') },
       { path: '/confirmarEmail', component: () => import('pages/confirmarEmail.vue') },
       { path: '/recuperarSenha', component: () => import('pages/recuperarSenha.vue') },
-      { path: '/cadastroAgenda', component: () => import('pages/cadastroAgenda.vue') }
+      { path: '/cadastroAgenda', component: () => import('pages/cadastroAgenda.vue') },
+      { path: '/resetAdm', component: () => import('pages/resetAdm.vue') }
     ]
   },
 
