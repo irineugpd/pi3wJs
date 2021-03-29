d4b6509e29b9e7d745cbc0ffc24dd59e45face12
IriPeres <irineugpd@unipam.edu.br>
2021-03-29 18:42:37 -0300

    Alteração da Index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,10 +13,8 @@
           </q-card-section>
           <q-card-section>
             <q-form class="q-px-sm q-pt-xl q-pb-lg">
-              <q-input class="q-pa-sm" standout v-model="name" placeholder="Nome"/>
-              <q-input class="q-pa-sm" standout v-model="email" placeholder="E-mail"/>
-              <q-input class="q-pa-sm" standout v-model="age" placeholder="Idade"/>
-              <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" label="Senha">
+              <q-input filled v-model="email" label="E-mail" class="q-pa-sm"/>
+              <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" label="Senha" class="q-pa-sm">
                 <template v-slot:append>
                   <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer q-pa-sm" standout @click="isPwd = !isPwd"/>
                 </template>
@@ -46,18 +44,14 @@ export default {
   data () {
     return {
       isPwd: true,
-      name: '',
       email: '',
-      age: 0,
       password: ''
     }
   },
   methods: {
     async login () {
       const params = {
-        name: this.name,
         email: this.email,
-        age: this.age,
         password: this.password
       }
       const response = await api.post('/users', params)
