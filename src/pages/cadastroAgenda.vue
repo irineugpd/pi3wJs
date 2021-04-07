ef819064bd0af36492e9c9a24aee1b2999f79298
Joao Mateus <joaomateusabm@yahoo.com.br>
2021-04-07 19:44:59 -0300

    added cadastroAgenda request

--- src/pages/cadastroAgenda.vue
+++ src/pages/cadastroAgenda.vue
@@ -46,15 +46,18 @@ export default {
   methods: {
     async register () {
       const params = {
-        name_event: this.name_event,
-        description: this.description,
-        event_date: this.event_date,
-        owner_id: '60a23d26-2d2b-4827-b20a-fa77385ea659'
+        name: this.name_event,
+        message: this.description,
+        date: this.event_date,
+        user_id: '60a23d26-2d2b-4827-b20a-fa77385ea658'
 
       }
-      const response = await api.post('/events', params)
-
-      console.log(response.data)
+      try {
+        const response = await api.post('/appointments', params)
+        console.log(response.data)
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 }
