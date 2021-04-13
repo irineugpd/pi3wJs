4cb10990e2f4e5c8482f3d3192407404e3043f78
Ruan Silva <71744272+Superhardcgs@users.noreply.github.com>
2021-04-13 11:44:02 -0300

    Update calendario.vue
    calendario puxando data atual, e com a primeira parte do v-for.

--- src/pages/calendario.vue
+++ src/pages/calendario.vue
@@ -1,80 +1,55 @@
 <template>
   <q-page class="window-height window-width row justify-center items-center" style="background: white;">
     <div class="column q-pa-lg">
-      <q-card square class="shadow-24" style="width:400px;height:600px;">
-        <q-card-section class="registroAnimal">
-          <h4 class="text-h5 text-white q-my-sm">Eventos</h4>
-          <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
-            <q-btn fab icon="list" color="green-13" />
-          </div>
-        </q-card-section>
-        <q-card-section>
-          <div v-scroll="scrolled" class="scroll overflow-hidden">
+      <center>
+        <h4 class="text-h5 text-black q-my-sm">Eventos</h4>
+      </center>
+        <center>
+          <div class="overflow-hidden">
             <div class="q-pa-md">
-              <q-date v-model="date" :events="event_date" event-color="black"/>
+              <q-date v-model="date" today-btn :events="event_date" event-color="black"/>
             </div>
-            <q-tab-panels v-model="date" animated transition-prev="jump-up" transition-next="jump-up">
-              <q-tab-panel name="2021/04/01">
-                <div class="text-h4 q-mb-md">2021/04/01</div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <q-card style="max-width: 75%;">
+            <q-tab-panels v-model="date" animated>
+              <q-tab-panel v-for="event_date in event_dates" :key="event_date" :name="event_date">
+                <div v-for="name_event in name_events" :key="name_event" class="q-mb-md">
+                  <h4 class="text-h4">{{ name_event }}</h4>
+                  <p v-for="description in descriptions" :key="description">{{description}}</p>
+                </div>
               </q-tab-panel>
-              <q-tab-panel name="2021/04/05">
-                <div class="text-h4 q-mb-md">2021/04/05</div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-                </q-tab-panel>
-              <q-tab-panel name="2021/04/06">
-                <div class="text-h4 q-mb-md">2021/04/06</div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-              </q-tab-panel>
-            </q-tab-panels>
-  </div>
-        </q-card-section>
-        <q-card-section class="text-center q-pa-sm">
+              </q-tab-panels>
+              </q-card>
+          </div>
+        </center>
+        <center>
           <q-btn flat style="color: gray" label="Retornar para LogIn" size="11px" to="/"/>
-          </q-card-section>
-        </q-card>
+        </center>
     </div>
   </q-page>
 </template>
 
 <script>
-import {
-  api
-} from 'boot/axios'
+import { api } from 'boot/axios'
+import { date } from 'quasar'
+
+const todayDate = Date.now()
+const formattedDate = date.formatDate(todayDate, 'YYYY/MM/DD', { })
 
 export default {
   name: 'cadastroAgenda',
   data () {
     return {
-      date: '2021/04/01',
-      event_date: ['2021/04/01', '2021/04/05', '2021/04/06']
+      date: formattedDate,
+      name_events: ['Evento 1', 'Evento 2', 'Evento 3'],
+      descriptions: ['Descrição 1', 'Descrição 2', 'Descrição 3 '],
+      event_dates: ['2021/04/01', '2021/04/05', '2021/04/06']
     }
   },
   methods: {
-    scrolled (position) {
-      // when this method is invoked then it means user
-      // has scrolled the page to `position`
-      //
-      // `position` is an Integer designating the current
-      // scroll position in pixels.
-    },
-    /* onLoad (index, done) {
-      setTimeout(() => {
-        if (this.event_date) {
-          this.event_date.push({}, {})
-          done()
-        }
-      }, 2000)
-    },
-    */
     async register () {
       const params = {
-        // name_event: this.name_event,
-        // description: this.description,
+        name_event: this.name_event,
+        description: this.description,
         event_date: this.event_date,
         owner_id: '60a23d26-2d2b-4827-b20a-fa77385ea659'
 
