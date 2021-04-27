303c624f9b66284a11f1efed2d22c5ce97d8f229
Ruan Silva <71744272+Superhardcgs@users.noreply.github.com>
2021-04-27 17:52:34 -0300

    Add files via upload

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="hHh lpR lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginLayout'
+}
+</script>
