49c7a3f75ab3c6d5ce8195d4e703c9ae07dcd374
Ruan Silva <71744272+Superhardcgs@users.noreply.github.com>
2021-05-11 12:59:55 -0300

    Atualização da página com informações e métodos

--- src/pages/recuperarSenha.vue
+++ src/pages/recuperarSenha.vue
@@ -1,48 +1,50 @@
 <template>
-  <q-page
-    class="justify-center items-center"
-    style="background: white;"
-  >
-    <div class="column q-pa-lg">
-        <q-card square class="shadow-24" style="width:auto;height:auto;">
-          <q-card-section class="registroAnimal">
-            <h4 class="text-h5 text-white q-my-sm">Recuperar Senha</h4>
-            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
-              <q-btn fab icon="list" color="green-13" />
-            </div>
-          </q-card-section>
-          <q-card-section>
-            <q-form class="q-px-sm q-pt-xl q-pb-lg">
-              <h6 class="text-h6 text-black q-my-sm">Digite a nova senha:</h6>
-              <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" label="Senha">
-                <template v-slot:append>
-                  <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer q-pa-sm" standout @click="isPwd = !isPwd"/>
-                </template>
-              </q-input>
-              <h6 class="text-h6 text-black q-my-sm">Repita a senha:</h6>
-              <q-input v-model="password2" filled :type="isPwd2 ? 'password' : 'text'" label="Repita a senha" color="teal-10">
-                <template v-slot:append>
-                  <q-icon :name="isPwd2 ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd2 = !isPwd2"/>
-                </template>
-              </q-input>
-            </q-form>
-          </q-card-section>
-          <q-card-actions class="q-px-lg">
-            <q-btn unelevated size="lg" color="green-13" class="full-width text-white" label="Solicitar" @click="solicitar()" />
-          </q-card-actions>
-          <q-card-section class="text-center q-pa-sm">
-            <q-btn flat style="color: gray" label="Retornar para LogIn" size="11px" to="/Login" color="teal-10"/>
-          </q-card-section>
-        </q-card>
-    </div>
+  <q-page class="q-pa-md">
+      <center>
+        <h4 class="loginTitle">RECUPERAÇÃO DE SENHA</h4>
+      </center>
+      <div class="absolute-bottom q-pb-md">
+        <q-form class="q-px-sm q-pb-xl">
+          <q-input
+            v-model="password"
+            filled :type="isPwd ? 'password' : 'text'"
+            label="Senha"
+            class="q-pa-md"
+            :rules="[val => val.length || 'Insira sua senha']"
+            color="teal-10"
+          >
+            <template v-slot:append>
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer q-pa-sm" standout @click="isPwd = !isPwd"/>
+            </template>
+          </q-input>
+          <q-input
+            v-model="password2"
+            filled :type="isPwd2 ? 'password' : 'text'"
+            label="Confirme a senha"
+            class="q-pa-md"
+            :rules="[val => val.length || 'Confirme a sua senha']"
+            color="teal-10"
+          >
+            <template v-slot:append>
+              <q-icon :name="isPwd2 ? 'visibility_off' : 'visibility'" class="cursor-pointer q-pa-sm" standout @click="isPwd2 = !isPwd2"/>
+            </template>
+          </q-input>
+        </q-form>
+        <center>
+          <q-btn unelevated rounded color="green-13" label="Confirmar" class="q-px-lg" @click="recuperar()" />
+          <q-btn flat color="gray-8" label="Voltar para o Login" to="/Login"/>
+        </center>
+      </div>
   </q-page>
 </template>
 
 <script>
-import { api } from 'boot/axios'
+import {
+  api
+} from 'boot/axios'
 
 export default {
-  name: 'recuperarSenha',
+
   data () {
     return {
       isPwd: true,
@@ -52,14 +54,21 @@ export default {
     }
   },
   methods: {
-    async solicitar () {
+    async recuperar () {
       if (this.password.length && this.password2.length > 0) {
-        const params = {
-          password: this.password
+        if (this.password === this.password2) {
+          const params = {
+            password: this.password
+          }
+          const response = await api.post('/users', params)
+          console.log(response.data)
+        } else {
+          this.$q.notify({
+            type: 'negative',
+            message: 'As senhas devem ser iguais.',
+            position: 'top'
+          })
         }
-        const response = await api.post('/recuperarSenha', params)
-
-        console.log(response.data)
       } else {
         this.$q.notify({
           type: 'negative',
