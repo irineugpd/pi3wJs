1d6c24d5dabf609ae2020090fa10a44bc10b6d90
Ruan Silva <71744272+Superhardcgs@users.noreply.github.com>
2021-05-13 12:54:24 -0300

    alterações visuais

--- src/pages/amostraAnimais.vue
+++ src/pages/amostraAnimais.vue
@@ -4,94 +4,84 @@
     style="background: white;"
   >
     <div class="column q-pa-lg">
-      <q-card square class="shadow-24" style="width:auto;height:auto;">
-        <q-card-section class="registroAnimal">
-          <h4 class="text-h5 text-white q-my-sm">Amostra de Animais:</h4>
-          <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+      <center>
+        <h5>Perfil do Animal:</h5>
+      </center>
+      <center>
+        <div class="q-pa-md">
+          <div class="q-pa-md">
+              <q-uploader
+              :factory="factoryFn"
+              multiple
+              style="max-width: 300px"
+              />
           </div>
-        </q-card-section>
-        <br>
-    <center>
-      <div class="col-6">
-            <q-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGrwExi5piK1AWFndmZDORgr3JGZPx4a51QQ&usqp=CAU" native-context-menu>
-          <div class="absolute-bottom-right text-subtitle2">
-            Junin Cavalus
-          </div>
-        </q-img>
-      </div>
-    </center>
-        <center>
+        </div>
+      </center>
+      <center>
         <div class="q-pa-md">
           <div class="q-gutter-y-md column" style="max-width: 300px">
             <div class="text-bold">
-              <h5 class="text-h5 text-black q-my-sm">Nome:</h5>
+              <h5>NOME:</h5>
+            </div>
             <p>
-              {{this.horse.name}}
+              {{horseName}}
             </p>
           </div>
         </div>
         <div class="q-pa-md">
-            <h5 class="text-h5 text-black q-my-sm">Pelagem:</h5>
-          <div class="q-gutter-y-md column" style="max-width: 200px">
+          <h5>PELAGEM:</h5>
+          <div class="q-gutter-y-md column" style="max-width: 300px">
             <p>
-              {{this.horse.fluff}}
+              {{fluffy}}
             </p>
           </div>
         </div>
         <div class="q-pa-md">
-            <h5 class="text-h5 text-black q-my-sm">Data de Nascimento:</h5>
-          <div class="q-gutter-y-md column" style="max-width: 200px">
+          <h5>DATA DE NASCIMENTO:</h5>
+          <div class="q-gutter-y-md column" style="max-width: 300px">
             <p>
-              {{this.horse.birth_date}}
+              {{birthDate}}
             </p>
           </div>
         </div>
         <div class="q-pa-md">
-            <h5 class="text-h5 text-black q-my-sm">Raça do Animal:</h5>
-          <div class="q-gutter-y-md column" style="max-width: 200px">
+          <h5>RAÇA DO ANIMAL:</h5>
+          <div class="q-gutter-y-md column" style="max-width: 300px">
             <p>
-              {{this.horse.race}}
+              {{racaAnimal}}
             </p>
           </div>
         </div>
-        </div>
-        </center>
-          <q-card-section class="text-center q-pa-sm">
-              <q-btn flat style="color: gray" label="Retornar para LogIn" size="11px" to="/"/>
-          </q-card-section>
-      </q-card>
+      </center>
+      <center>
+        <q-btn unelevated size="lg" color="primary" filled rounded class="center text-white" label="Salvar perfil" @click="salvar()" />
+      </center>
     </div>
   </q-page>
 </template>
 
 <script>
-import { api } from 'boot/axios'
+import {
+} from 'boot/axios'
 
 export default {
   name: 'amostraAnimais',
   data () {
     return {
-      horse: {}
+      horseName: 'Junin Pangaré',
+      fluffy: 'Preta',
+      birthDate: '10/05/2015',
+      racaAnimal: 'Manga Larga',
+      filesImages: null,
+      dense: false
     }
   },
   methods: {
-    async getHorse (id) {
-      api.defaults.headers.authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MjA3MzU0MzcsImV4cCI6MTYyMzMyNzQzNywic3ViIjoie1wiaWRcIjpcImY1ODU0OGQ2LWU5MWQtNGUwOC1iMWYyLTIyZWI4OTJhM2Y4OFwiLFwiaXNfYWRtaW5pc3RyYXRvclwiOnRydWV9In0.O923JprdiG5q2RKmzWQh41FYWV6fqM3oYVbEWAnQ-iQ'
-      try {
-        const response = await api.get(`/horses/${id}`)
-        this.horse = response.data
-        const birthDate = new Date(response.data.birth_date)
-        const year = birthDate.getFullYear()
-
-        console.log(year)
-      } catch (e) {
-        console.log(e)
-      }
+    factoryFn (files) {
+      this.filesimages = files
+      console.log(this.filesImages)
     }
-  },
-  async created () {
-    await this.getHorse('c84f48ba-a1f8-4097-aa96-07e01a37be64')
-    console.log(this.horse)
   }
 }
 </script>
