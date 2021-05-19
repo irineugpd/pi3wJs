502d137ccb6e4a6dcc8948476be66bf92cedbc45
IriPeres <irineugpd@unipam.edu.br>
2021-05-19 13:49:37 -0300

    ...

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,5 +1,41 @@
 <template>
   <q-layout view="hHh Lpr lFr">
+    <q-header>
+      <q-toolbar  class="backgroundAdmDashboard">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          color="black"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <!--<p class="titleDashboard">CS.VET</p>-->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      overlay
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          CS.VET
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -7,8 +43,61 @@
 </template>
 
 <script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'LOGIN CS_VET APP',
+    caption: 'Login',
+    icon: 'account_box',
+    link: '#/Login'
+  },
+  {
+    title: 'CADASTRO ANIMAL',
+    caption: 'Cadastro de Animais',
+    icon: 'pets',
+    link: '#/cadastroAnimais'
+  },
+  {
+    title: 'CADASTRO DE VACINAS',
+    caption: 'Cadastro Vacina',
+    icon: 'medical_services',
+    link: '#/cadastroVacina'
+  },
+  {
+    title: 'CALENDÁRIO DE EVENTOS',
+    caption: 'Eventos marcados',
+    icon: 'event',
+    link: '#/calendario'
+  },
+  {
+    title: 'AMOSTRA DE ANIMAIS',
+    caption: 'Lista de Animais',
+    icon: 'gesture',
+    link: '#/amostraAnimais'
+  },
+  {
+    title: 'CARTAO DE VACINA',
+    caption: 'Vacinas do Animal',
+    icon: 'dashboard_customize',
+    link: '#/cartaoVacina'
+  },
+  {
+    title: 'LISTA DE ANIMAIS',
+    caption: 'Perfil de Animais',
+    icon: 'list',
+    link: '#/listarAnimais'
+  }
+]
 
 export default {
-  name: 'AdminLayout.vue'
+  name: 'AdmLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
 }
 </script>
---
+++ src/pages/UserList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="q-pa-md" style="width:auto">
+    <q-list bordered>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <q-icon color="primary" name="perm_identity" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>Image avatar</q-item-section>
+      </q-item>
+      <q-separator />
+    </q-list>
+  </div>
+</template>
