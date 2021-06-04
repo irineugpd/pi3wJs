a83c04feaff0b36c719810651efc89c4da9486cd
João Mateus <joaomateusabm@yahoo.com.br>
2021-06-04 13:32:18 -0300

    chore: Connected some screen with the backend an implemented the verify is authenticated feature

---
+++ src/utils/helper.vue
@@ -0,0 +1,18 @@
+<script>
+import { api } from 'src/boot/axios'
+export default {
+  methods: {
+    verifyIsAuthenticated () {
+      const user = JSON.parse(this.$q.localStorage.getItem('@AppCamila:User'))
+      const token = JSON.parse(this.$q.localStorage.getItem('@AppCamila:Token'))
+
+      if (user && token) {
+        api.defaults.headers.authorization = `Bearer ${token}`
+        this.$router.push('/menu')
+      } else {
+        this.$router.push('/login')
+      }
+    }
+  }
+}
+</script>
